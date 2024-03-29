9cede293b910e8150e56aa80fe1114f74f4825e9
LazyR9 <72282692+LazyR9@users.noreply.github.com>
2024-03-29 18:19:14 -0500

    Update main.py
    just a few lines I just don't want these uncommited changes sitting around forever lol

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from app.management.manager import ServerManager
 
+ServerManager.load_builtin_games()
 manager = ServerManager()
 manager.load_settings()
 manager.load_servers()
 
 # TODO finish writing main.py once everything is in a workable state lol
+print(manager.servers)
+
+# TODO replace use propper logging instead of print statements
