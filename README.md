c73827565d50c00f054165fe0f3a6722c9021d89
LazyR9 <72282692+LazyR9@users.noreply.github.com>
2024-07-13 10:45:58 -0500

    Add installer

--- README.md
+++ README.md
@@ -27,6 +27,37 @@ You can also provide custom pages to the frontend to show extra game details and
 
 ## Setup
 
-Right now setting up for both end users and other developers isn't very easy right now,
-because some necessary files aren't automatically generated.
-Hopefully that changes soon!
+### Development
+Just clone the repo and open in VS Code, there are tasks and debug configurations already setup.
+
+### Deployment
+
+<!-- Through trial and error I found 3.10 is the minimum version of Python needed,
+but I don't know what version of npm is needed. -->
+* Requires at least Python 3.10 to run the backend, and npm 10.2.5 to compile the frontend.
+
+* Requires a reverse proxy to serve the backend API and frontend on the same address, and optionally a program to autostart the API on boot.
+    * The install script detects Ubuntu-like nginx and supervisor configs (`sites-available/` and `sites-enabled/` in `/etc/nginx/` and `conf.d/` in `/etc/supervisor/`)
+    and will automatically install files to these directories.
+
+After cloning the repo, just run `install.py` with sudo and it will automatically deploy to any supported configs.
+
+If you don't have a supported config, you can manually deploy using these steps:
+1. Setup virtual envrionment
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+2. Build frontend
+```bash
+cd app/webapp/frontend
+npm install
+npm build
+```
+3. Configure your reverse proxy/process manager.
+
+    The backend can be started by running `venv/bin/python3 main.py` with this directory as the current working directory.
+    `main.py` also takes some arguments, `--port` specifies what port it should be run on, and `--directory` specifies what directory files should be stored in.
+
+    Any requests to `/api` should be proxied to the backend, otherwise just serve the frontend (all routing is done client side)
---
+++ install.py
@@ -0,0 +1,132 @@
+import platform
+import shutil
+import os
+import subprocess
+import venv
+import string
+
+CONF_NAME = "lazy_game_servers.conf"
+
+MANUAL_INSTALL_INFO = """
+Any values wrapped with ${} in the config are replaced with a value by the installer.
+When manually installing, you will need to substitute the appropriate values yourself.
+Any double dollar signs ($$) should also be replaced by a single dollar sign.
+"""
+
+_cmd_installed_cache = {}
+def cmd_installed(cmd):
+    if cmd not in _cmd_installed_cache:
+        _cmd_installed_cache[cmd] = shutil.which(cmd) is not None
+    return _cmd_installed_cache[cmd]
+
+def copy_with_replacements(input_filename, output_filename, replacements):
+    with open(input_filename, "r") as input_file:
+        input_file_contents = input_file.readlines()
+    output_file_contents = []
+    for line in input_file_contents:
+        template = string.Template(line)
+        output_file_contents.append(template.substitute(replacements))
+    with open(output_filename, "w") as output_file:
+        output_file.writelines(output_file_contents)
+
+if platform.system() == "Windows":
+    print("Installer not available on Windows yet!")
+    print("Try installing on Linux instead!")
+    exit(-1)
+
+# map of dependency commands to whether or not they are required
+DEPENDENCIES = {
+    "nginx": False,
+    "supervisord": False,
+    "npm": True
+}
+
+for dependecy, required in DEPENDENCIES.items():
+    if not cmd_installed(dependecy):
+        print(f"Dependecy \"{dependecy}\" was not found!")
+        if required:
+            print("Please make sure it is installed and available on PATH and try again")
+            exit(-1)
+        else:
+            print("We will skip the part of the installer that requires it!")
+
+EXTERNAL_PORT = 80
+INTERNAL_PORT = 8000
+
+FRONTEND_ASSETS_PATH = "/var/www/html/lazy_game_servers"
+
+CONFIG_REPLACABLE = {
+    "CWD": os.getcwd(),
+    "EXTERNAL_PORT": EXTERNAL_PORT,
+    "INTERNAL_PORT": INTERNAL_PORT,
+    "FRONTEND_ASSETS_PATH": FRONTEND_ASSETS_PATH,
+}
+
+print("Creating venv...")
+
+# symlinks should be False on Windows, but this script doesn't run on Windows so it's just hardcoded
+venv.create("venv", symlinks=True, with_pip=True)
+subprocess.run(["venv/bin/pip", "install", "-r", "requirements.txt"])
+
+print("Building frontend...")
+
+NPM_DIR = "app/webapp/frontend"
+
+subprocess.run(["npm", "install"], cwd=NPM_DIR)
+npm_build = subprocess.run(["npm", "run", "build"], cwd=NPM_DIR)
+if npm_build.returncode != 0:
+    print("Non zero return code from running \"npm run build\"!")
+    exit(-1)
+
+if os.path.exists(FRONTEND_ASSETS_PATH):
+    print("Old frontend build found, removing...")
+    shutil.rmtree(FRONTEND_ASSETS_PATH)
+print("Moving frontend build to", FRONTEND_ASSETS_PATH)
+shutil.move("app/webapp/frontend/build", FRONTEND_ASSETS_PATH)
+
+NGINX_SITES_AVAILABLE = '/etc/nginx/sites-available'
+NGINX_SITES_ENABLED = '/etc/nginx/sites-enabled'
+
+NGINX_CONF_NAME = CONF_NAME
+
+if cmd_installed("nginx"):
+    if os.path.exists(NGINX_SITES_AVAILABLE) and os.path.exists(NGINX_SITES_ENABLED):
+        print("Found nginx sites-available and sites-enabled directories, auto installing and enabling nginx config")
+
+        if os.path.exists(NGINX_SITES_ENABLED + "/default"):
+            print("WARNING: default nginx conf is still enabled!")
+            print("You may need to remove this if you are using the default port,")
+            print("  and probably will want to remove it anyway")
+        
+        abs_conf_path = NGINX_SITES_AVAILABLE + "/" + NGINX_CONF_NAME
+
+        copy_with_replacements("default_configs/nginx.conf", abs_conf_path, CONFIG_REPLACABLE)
+
+        enabled_path = NGINX_SITES_ENABLED + "/" + NGINX_CONF_NAME
+        if not os.path.exists(enabled_path):
+            os.symlink(abs_conf_path, enabled_path)
+
+        subprocess.run(["nginx", "-s", "reload"])
+
+        print("Successfully added and enabled nginx config")
+    else:
+        print("No supported nginx config found!")
+        print("Please manually install the config found in default_configs/nginx.conf")
+        print(MANUAL_INSTALL_INFO)
+
+SUPERVISOR_CONF_D = "/etc/supervisor/conf.d"
+SUPERVISOR_CONF_NAME = CONF_NAME
+
+if cmd_installed("supervisord"):
+    if os.path.exists(SUPERVISOR_CONF_D):
+        print("Found supervisor conf.d directory, auto installing supervisor config")
+
+        copy_with_replacements("default_configs/supervisor.conf", SUPERVISOR_CONF_D + "/" + SUPERVISOR_CONF_NAME, CONFIG_REPLACABLE)
+
+        subprocess.run(["supervisorctl", "reload"])
+    else:
+        print("No supported supervisor config found!")
+        print("Please manually install the config found in default_configs/supervisor.conf")
+        print(MANUAL_INSTALL_INFO)
+
+print("Finished setup!")
